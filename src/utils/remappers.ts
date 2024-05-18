e13cbc8c31249ce7a7d3f1d9a951e03a2055d08d
NicolasAversa <nicolasaversadm@gmail.com>
2024-05-18 14:37:27 -0300

    chore: :hammer: Create remappers utils

---
+++ src/utils/remappers.ts
@@ -0,0 +1,46 @@
+import {
+  WeatherInformation,
+  WeatherForecast,
+} from "@/context/weatherContext/WeatherContext";
+import { CurrentWeatherApiResponse, ForecastApiResponse } from "@/types";
+import { parse } from "date-fns";
+
+const remapWeatherInformation = (
+  weather: CurrentWeatherApiResponse
+): WeatherInformation => {
+  return {
+    cloudPercentage: weather.current.cloud,
+    humidity: weather.current.humidity,
+    temperature: {
+      celsius: weather.current.temp_c,
+      fahrenheit: weather.current.feelslike_f,
+    },
+    windSpeed: {
+      kilometersPerHour: weather.current.wind_kph,
+      metersPerSecond: weather.current.wind_mph,
+    },
+    windDirection: weather.current.wind_degree,
+  };
+};
+
+const remapForecast = (weather: ForecastApiResponse): WeatherForecast[] => {
+  const forecast: WeatherForecast[] = weather.forecast.forecastday.map(
+    ({ date, day }) => ({
+      date: parse(date, "yyyy-MM-dd", new Date()),
+      cloudPercentage: day.daily_chance_of_rain,
+      humidity: day.avghumidity,
+      temperature: {
+        celsius: day.avgtemp_c,
+        fahrenheit: day.avgtemp_f,
+      },
+      windSpeed: {
+        kilometersPerHour: day.maxwind_kph,
+        metersPerSecond: day.maxwind_mph,
+      },
+    })
+  );
+
+  return forecast;
+};
+
+export { remapWeatherInformation, remapForecast };

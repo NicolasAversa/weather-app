b7c1f76ad758bd280fb0d8c735f66df93969be37
NicolasAversa <nicolasaversadm@gmail.com>
2024-05-18 15:24:35 -0300

    feat: :sparkles: Create WeatherIcon component

---
+++ src/components/compositions/shared/WeatherIcon.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import clouds from "@/../public/icons/weatherTypes/clouds.svg";
+import cloudy from "@/../public/icons/weatherTypes/cloudy.svg";
+import raining from "@/../public/icons/weatherTypes/raining.svg";
+import sunny from "@/../public/icons/weatherTypes/sunny.svg";
+import { WeatherTypes } from "@/types";
+
+enum IconSize {
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+}
+interface WeatherIconProps {
+  type: `${WeatherTypes}`;
+  size?: `${IconSize}`;
+}
+
+const iconSizes: Record<IconSize, number> = {
+  [IconSize.SMALL]: 32,
+  [IconSize.MEDIUM]: 80,
+  [IconSize.LARGE]: 128,
+};
+
+const weatherTypesIcons: Record<WeatherTypes, string> = {
+  [WeatherTypes.SUNNY]: sunny,
+  [WeatherTypes.CLOUDY]: cloudy,
+  [WeatherTypes.CLOUDS]: clouds,
+  [WeatherTypes.RAIN]: raining,
+};
+
+function WeatherIcon({ type, size = "medium" }: WeatherIconProps) {
+  return (
+    <Image
+      src={weatherTypesIcons[type]}
+      alt={`${type} weather`}
+      height={iconSizes[size]}
+    />
+  );
+}
+
+export { WeatherIcon };

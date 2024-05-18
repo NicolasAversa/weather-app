87bfddbaa76fdbcd037db4b384c443c7ab1a9595
NicolasAversa <nicolasaversadm@gmail.com>
2024-05-18 14:52:02 -0300

    chore: :wrench: Create typography constants

---
+++ src/components/base/typography/constants.ts
@@ -0,0 +1,9 @@
+import { FontWeight } from "./types";
+
+const fontWeights = {
+  [FontWeight.REGULAR]: 300,
+  [FontWeight.MEDIUM]: 400,
+  [FontWeight.SEMI_BOLD]: 500,
+};
+
+export { fontWeights };

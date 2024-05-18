0d9068814a44f8c860661dbdc59e74059a2cb498
NicolasAversa <nicolasaversadm@gmail.com>
2024-05-18 15:26:36 -0300

    chore: :technologist: Add respective component compositions barrel file

---
+++ src/components/compositions/index.ts
@@ -0,0 +1,6 @@
+export * from "./city/Forecast";
+export * from "./shared/CurrentWeather";
+export * from "./shared/CurrentWeatherDisplay";
+export * from "./shared/FavoriteButton";
+export * from "./shared/WeatherIcon";
+export * from "./home/FavoriteCitySection";

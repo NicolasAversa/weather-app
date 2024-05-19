d4f12bf7cee93091459deb2bb64b9ebe9b401652
NicolasAversa <nicolasaversadm@gmail.com>
2024-05-19 18:17:11 -0300

    chore: :wrench: Add barrel for context folder

---
+++ src/context/index.ts
@@ -0,0 +1,4 @@
+export * from "./weatherContext/WeatherContext";
+export * from "./weatherContext/WeatherProvider";
+export * from "./weatherContext/hooks/useWeatherContext";
+export * from "./weatherContext/weatherReducer";

4195c333c945ca9185e5fe61eb76d95286a42dd2
NicolasAversa <nicolasaversadm@gmail.com>
2024-05-19 18:21:56 -0300

    style: :lipstick: Add more styles to Text component

--- src/components/base/typography/Text.tsx
+++ src/components/base/typography/Text.tsx
@@ -2,12 +2,15 @@ import { Typography } from "@mui/material";
 import { CommonTypographyProps } from "./types";
 import { fontWeights } from "./constants";
 
-interface TextProps extends CommonTypographyProps {}
+interface TextProps extends CommonTypographyProps {
+  as?: "p" | "span";
+}
 
-function Text({ fontWeight, color = "#2C2C2C", ...rest }: TextProps) {
+function Text({ fontWeight, color = "#2C2C2C", as = "p", ...rest }: TextProps) {
   return (
     <Typography
       {...rest}
+      component={as}
       variant="body1"
       color={color}
       fontWeight={fontWeight ? fontWeights[fontWeight] : undefined}

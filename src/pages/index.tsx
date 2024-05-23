81e87b791886b56d245e40a8ed41759bb8d0a3ae
Nicolas Aversa <64762111+NicolasAversa@users.noreply.github.com>
2024-05-23 12:17:29 -0700

    Feature/Implement HomeSkeleton component (#7)
    * fix: :adhesive_bandage: Adjust humidity threshold in getWeatherTypeFromWeather
    
    * chore: :truck: Move FavoriteCitySection to its own folder
    
    * style: :lipstick: Add skeleton to HomeSkeleton component
    
    * feat: :sparkles: Implement HomeSkeleton in Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { fetchWeatherByCityName } from "@/utils/api";
 import {
   RealtimeWeatherReport,
   DetailedWeatherInformation,
+  HomeSkeleton,
 } from "@/components/compositions";
 import { useAutoDetectClientCity } from "@/hooks";
 import { format } from "date-fns";
@@ -13,6 +14,8 @@ import { ROUTES, dateFormats } from "@/constants";
 export default function Home() {
   const [inputCity, setInputCity] = useState<string>();
   const [city, setCity] = useState<string>();
+  const [isFetchingCityWeather, setIsFetchingCityWeather] =
+    useState<boolean>(false);
   const {
     state: { locations },
     dispatchers: { setLocationWeather },
@@ -30,11 +33,17 @@ export default function Home() {
 
   useEffect(() => {
     (async function () {
-      if (city && !isCityWeatherCached(city)) {
-        const weatherResponse = await fetchWeatherByCityName(city);
-        if (weatherResponse) {
-          setLocationWeather(city, weatherResponse);
+      try {
+        if (city && !isCityWeatherCached(city)) {
+          setIsFetchingCityWeather(true);
+          const weatherResponse = await fetchWeatherByCityName(city);
+          setIsFetchingCityWeather(false);
+
+          if (weatherResponse) setLocationWeather(city, weatherResponse);
         }
+      } catch (error) {
+        setIsFetchingCityWeather(false);
+        console.error(error);
       }
     })();
   }, [city]);
@@ -46,6 +55,8 @@ export default function Home() {
   const handleCitySearchClick = () => {
     setCity(inputCity);
   };
+
+  const isWeatherReportAvailable = !isFetchingCityWeather && cityWeatherReport;
 
   return (
     <Stack direction="column" spacing={3}>
@@ -60,41 +71,42 @@ export default function Home() {
           Search
         </Button>
       </Stack>
-      {cityWeatherReport ? (
-        <RealtimeWeatherReport weather={cityWeatherReport} />
+      {!isWeatherReportAvailable ? <HomeSkeleton /> : null}
+      {isWeatherReportAvailable ? (
+        <>
+          <RealtimeWeatherReport weather={cityWeatherReport} />
+          <DetailedWeatherInformation
+            items={[
+              {
+                label: "Local time",
+                value: format(
+                  cityWeatherReport.localTime,
+                  dateFormats.hourMinutes
+                ),
+              },
+              {
+                label: "Wind speed",
+                value: `${cityWeatherReport.windSpeed.kilometersPerHour} Km/h`,
+              },
+              {
+                label: "Humidity",
+                value: `${cityWeatherReport.humidity}%`,
+              },
+              {
+                label: "Clouds",
+                value: `${cityWeatherReport.cloudPercentage}%`,
+              },
+            ]}
+          />
+          <LinkButton
+            href={`${ROUTES.CITY_DETAILS}/${city}`}
+            variant="outlined"
+            fullWidth
+          >
+            View detailed weather
+          </LinkButton>
+        </>
       ) : null}
-      {cityWeatherReport ? (
-        <DetailedWeatherInformation
-          items={[
-            {
-              label: "Local time",
-              value: format(
-                cityWeatherReport.localTime,
-                dateFormats.hourMinutes
-              ),
-            },
-            {
-              label: "Wind speed",
-              value: `${cityWeatherReport.windSpeed.kilometersPerHour} Km/h`,
-            },
-            {
-              label: "Humidity",
-              value: `${cityWeatherReport.humidity}%`,
-            },
-            {
-              label: "Clouds",
-              value: `${cityWeatherReport.cloudPercentage}%`,
-            },
-          ]}
-        />
-      ) : null}
-      <LinkButton
-        href={`${ROUTES.CITY_DETAILS}/${city}`}
-        variant="outlined"
-        fullWidth
-      >
-        View detailed weather
-      </LinkButton>
     </Stack>
   );
 }
--- src/utils/weather.ts
+++ src/utils/weather.ts
@@ -2,7 +2,7 @@ import { Weather, WeatherTypes } from "@/types";
 
 const getWeatherTypeFromWeather = (weather: Weather): WeatherTypes => {
   const { cloudPercentage, humidity } = weather;
-  if (humidity >= 70 && cloudPercentage >= 75) return WeatherTypes.RAIN;
+  if (humidity >= 90 && cloudPercentage >= 75) return WeatherTypes.RAIN;
   if (cloudPercentage >= 75) return WeatherTypes.CLOUDS;
   if (cloudPercentage >= 25) return WeatherTypes.CLOUDY;
   return WeatherTypes.SUNNY;

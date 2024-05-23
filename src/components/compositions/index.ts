852346cddc0d2f846989e36e0ff876266a63dab7
NicolasAversa <nicolasaversadm@gmail.com>
2024-05-23 14:30:32 -0300

    chore: :truck: Export DegreesIndicator component

--- src/components/compositions/index.ts
+++ src/components/compositions/index.ts
@@ -3,4 +3,5 @@ export * from "./shared/CurrentWeather";
 export * from "./shared/CurrentWeatherDisplay";
 export * from "./shared/FavoriteButton";
 export * from "./shared/WeatherIcon";
+export * from "./shared/DegreesIndicator";
 export * from "./home/FavoriteCitySection";

3f5a6da0b05bc7f381a31871a49c0ef9ceed4cd9
NicolasAversa <nicolasaversadm@gmail.com>
2024-05-23 15:35:45 -0300

    feat: :sparkles: Implement Navbar component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,26 @@
 import { AppProps } from "next/app";
-import {
-  WeatherProvider,
-  initialValues,
-} from "@/context/weatherContext/WeatherProvider";
+import { WeatherProvider, initialValues } from "@/context";
 import { useLocalStorage } from "@/hooks";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "@/styles/theme";
+import { Container } from "@mui/material";
+import { Box } from "@/components/base";
+import { Navbar, NavbarProps } from "@/components/compositions";
+import { ROUTES } from "@/constants";
 import "@fontsource/metropolis/400.css";
 import "@fontsource/metropolis/500.css";
 import "@fontsource/metropolis/600.css";
+
+const NAVBAR_ITEMS: NavbarProps["pages"] = [
+  {
+    href: ROUTES.HOME,
+    label: "Home",
+  },
+  {
+    href: ROUTES.FAVORITE_CITIES,
+    label: "Favorite cities",
+  },
+];
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [favoriteCities] = useLocalStorage("favoriteCities", []);
@@ -21,7 +33,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       }}
     >
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <Navbar pages={NAVBAR_ITEMS} />
+        <Container maxWidth="sm">
+          <Box marginTop={3}>
+            <Component {...pageProps} />
+          </Box>
+        </Container>
       </ThemeProvider>
     </WeatherProvider>
   );

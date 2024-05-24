b3b52e42ecc51ea018ff7ba2acb65724f2d9c2e3
NicolasAversa <nicolasaversadm@gmail.com>
2024-05-24 15:43:53 -0300

    chore: :sparkles: Update routes constants

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -4,4 +4,13 @@ const ROUTES = {
   CITY_DETAILS: "/city",
 };
 
-export { ROUTES };
+const API_BASE_ROUTE = "/api";
+
+const API_ROUTES = {
+  autocomplete: `${API_BASE_ROUTE}/autocomplete`,
+  clientCity: `${API_BASE_ROUTE}/client-city`,
+  forecast: `${API_BASE_ROUTE}/forecast`,
+  weather: `${API_BASE_ROUTE}/weather`,
+};
+
+export { ROUTES, API_ROUTES };
